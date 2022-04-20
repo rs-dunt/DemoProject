3d927fca5e6e221503a5d13af0409ff3c61cc5c5
rs-huannd <dunt@runsystem.net>
2022-04-20 09:16:53 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/Theme'
+import {
+  SplashScreen,
+  LoginScreen,
+  RegisterScreen,
+} from './src/screens'
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="SplashScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export { default as SplashScreen } from './SplashScreen'
+export { default as LoginScreen } from './LoginScreen'
+export { default as RegisterScreen } from './RegisterScreen'
+// export { default as ResetPasswordScreen } from './ResetPasswordScreen'
+// export { default as Dashboard } from './Dashboard'
