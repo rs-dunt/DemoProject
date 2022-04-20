cd46708236dc860178ac3ca1bc406550161a544a
rs-huannd <dunt@runsystem.net>
2022-04-20 09:39:31 +0700

    add background

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
 import React from 'react'
 import { Provider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { theme } from './src/core/Theme'
 import {
   SplashScreen,
   LoginScreen,
   RegisterScreen,
+  Dashboard
 } from './src/screens'
 
-const Stack = createStackNavigator()
+const Stack = createNativeStackNavigator()
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="SplashScreen"
+//         screenOptions={{
+//           headerShown: false,
+//         }}
+//       >
+//         <Stack.Screen name="SplashScreen" component={SplashScreen} />
+//         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+//         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+
+//   )
+// }
+const App = () => {
   return (
-    <Provider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="SplashScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="SplashScreen" component={SplashScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SplashScreen"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
